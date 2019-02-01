6848719004fa32a301238ddd56574f9a899d4494
johnson <junhong1@ualberta.ca>
2019-01-31 18:43:35 -0700

    new z

--- app/src/main/java/com/example/lab4/Star.java
+++ app/src/main/java/com/example/lab4/Star.java
@@ -1,4 +1,12 @@
 package com.example.lab4;
 
-public class Star {
+public class Star extends Shape {
+    private int edges;
+    private int edge_length;
+
+    Star(int edges, int edge_length) {
+        this.edges = edges;
+        this.edge_length = edge_length;
+    }
+
 }
